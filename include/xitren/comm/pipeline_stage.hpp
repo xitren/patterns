df94e76e0e21e581f797d0f2a6ca09aa0e946b10
xitren <xitren@ya.ru>
2024-12-30 00:02:23 +0300

    Added measurement

--- include/xitren/comm/pipeline_stage.hpp
+++ include/xitren/comm/pipeline_stage.hpp
@@ -16,6 +16,7 @@ __ _(_) |_ _ _ ___ _ _
 #include <functional>
 #include <mutex>
 #include <optional>
+#include <queue>
 #include <ranges>
 #include <thread>
 #include <utility>
@@ -28,14 +29,18 @@ enum class pipeline_stage_exception : int { no_error = 0x00 };
 
 template <class Type, class NextType, std::size_t BufferSize, func::log_adapter_concept Log>
 class pipeline_stage {
+    static int const measure_points = 10;
+
     using atomic_cnt_type    = std::atomic<std::size_t>;
     using atomic_closed_type = std::atomic<bool>;
     using ready_type         = std::optional<Type>;
+    using measure_type       = std::pair<int, int>;
+    using statistics_type    = std::deque<measure_type>;
     using queue_type         = struct type_tag {
         std::array<ready_type, BufferSize> array;
         atomic_cnt_type                    tail;
     };
-    using function_type = std::function<const NextType(pipeline_stage_exception, const Type)>;
+    using function_type = std::function<const NextType(pipeline_stage_exception, const Type, const measure_type)>;
 
 public:
     pipeline_stage(function_type func) : func_{func} {}
@@ -43,7 +48,9 @@ public:
     ~pipeline_stage()
     {
         closed_ = true;
-        worker_.join();
+        if (worker_.joinable()) {
+            worker_.join();
+        }
     }
 
     void
@@ -56,11 +63,42 @@ public:
 #endif
     }
 
+    void
+    push(Type const& data)
+    {
+        auto const i                          = queue_.tail++;
+        queue_.array[i % queue_.array.size()] = data;
+#ifdef DEBUG
+        Log::trace() << "Index: " << queue_.tail << "\n";
+#endif
+    }
+
+    auto
+    time_for_unit() const
+    {
+        int calc{};
+        for (auto& item : stat_) {
+            calc += item.first;
+        }
+        return calc / measure_points;
+    }
+
+    auto
+    buffer_utilization() const
+    {
+        int calc{};
+        for (auto& item : stat_) {
+            calc += item.second;
+        }
+        return calc / measure_points;
+    }
+
 private:
     atomic_cnt_type    tail_{0};
     atomic_closed_type closed_{false};
     queue_type         queue_{};
     function_type      func_{};
+    statistics_type    stat_{};
 
     std::thread worker_ = std::thread{[this]() {
         using namespace std::chrono_literals;
@@ -81,7 +119,15 @@ private:
                 Log::trace() << "Index to process: " << i << "\n";
 #endif
                 auto& data = queue_.array[i % queue_.array.size()];
-                func_(pipeline_stage_exception::no_error, data.value());
+                auto  last_time{std::chrono::system_clock::now()};
+                func_(pipeline_stage_exception::no_error, data.value(),
+                      measure_type{time_for_unit(), buffer_utilization()});
+                auto elapsed = std::chrono::system_clock::now() - last_time;
+                stat_.push_front(measure_type{std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count(),
+                                              queue_.tail - tail_});
+                if (stat_.size() > measure_points) {
+                    stat_.pop_back();
+                }
             }
             std::this_thread::sleep_for(0ms);
         }
--- tests/patterns_pipeline_test.cpp
+++ tests/patterns_pipeline_test.cpp
@@ -38,16 +38,41 @@ TEST(pipeline_test, basic)
 {
     using pipeline_type = pipeline_stage<std::string, std::string, 1024, LogCout>;
     std::atomic<int> i{};
+    static auto      func
+        = [&](pipeline_stage_exception ex, const std::string str, const std::pair<int, int> stat) -> const std::string {
+        LogCout::debug() << "String: " << str << "\n";
+        i++;
+        return str;
+    };
     {
-        pipeline_type stage([&](pipeline_stage_exception ex, const std::string str) -> const std::string {
-            LogCout::debug() << "String: " << str << "\n";
-            i++;
-            return str;
-        });
+        pipeline_type stage(func);
         stage.push("First");
         stage.push("Second");
         stage.push("Third");
         LogCout::debug() << std::endl;
     }
     EXPECT_EQ(i, 3);
+}
+
+TEST(pipeline_test, basic_void_ret)
+{
+    using pipeline_type = pipeline_stage<std::string, void, 1024, LogCout>;
+    std::atomic<int> i{};
+    static auto func = [&](pipeline_stage_exception ex, const std::string str, const std::pair<int, int> stat) -> void {
+        LogCout::debug() << "String: " << str << " Time: " << stat.first << " Util: " << stat.second << "\n";
+        using namespace std::chrono;
+        i++;
+    };
+    {
+        pipeline_type stage(func);
+        stage.push("First");
+        stage.push("Second");
+        stage.push("Third");
+        for (int i{}; i < 100; i++) {
+            stage.push("Next");
+            stage.push("Other");
+        }
+        LogCout::debug() << std::endl;
+    }
+    EXPECT_EQ(i, 203);
 }
